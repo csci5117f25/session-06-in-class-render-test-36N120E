11f6cb68cb0b3d76f6eec1e6847da3d5e019cc0e
Yuankun <jiao0052@umn.edu>
2025-09-24 22:20:32 -0500

    init

--- server.py
+++ server.py
@@ -3,6 +3,9 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route('/')
-@app.route('/<name>')
+def index():
+    return "Hello from Render!"
+
+@app.route('/hello')
 def hello(name=None):
     return render_template('hello.html', name=name)
